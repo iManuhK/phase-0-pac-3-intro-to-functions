101d69bbb338e68a69f976cdb4b7667e6120cbc8
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-03-13 17:28:27 +0300

    Adds to index.js

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Follow along with the examples here
+// function doNothing() {   
+// }
+// function sayHelloGd (){
+//     console.log ("Hello, Guadalupe!");
+// }
+
+// function sayHelloLiz (){
+//     console.log ("Hello, Liz!");
+// }
+
+// function sayHelloTp (){
+//     console.log ("Hello, Tamapa!");
+// // }
+// // sayHelloGd();
+// // sayHelloLiz();
+// // sayHelloTp();
+
+// // function sayHelloTo(firstName) {
+// //     console.log(`Hello, ${firstName}!`);
+// //   }
+  
+// //   sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+// //   sayHelloTo("Jane"); // "Hello, Jane!"
+// //   sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+// //   sayHelloTo(1); // "Hello, 1!"
+// //   console.log(firstName);
+  
+// function say(greeting, name){
+// console.log(greeting, name)
+// }
+
+// say("Julio","Hi my good friend");
+// function add(x, y) {
+//     return x + y;
+//   }
+//   console.log(add(9,9));
+
+//   function say(greeting, firstName) {
+//     return `${greeting}, ${firstName}!`;
+//   }
+//   console.log(say("Hi", "Liz"));
+//   function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+// function add(x, y) {
+//    return x + y;
+//   }
+//   console.log(add(1, 2));
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+    
+  }
+  console.log("Howdy", "Partner")
+  
